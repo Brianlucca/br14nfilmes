f3a7abab25c6cbff7a64df1737a9705896076dc8
Brianlucca <brianlucca2002@gmail.com>
2024-08-08 02:34:32 -0300

    feat: enhance MovieItem component with default image handling and accessibility improvements

--- src/components/movies/MovieItem.jsx
+++ src/components/movies/MovieItem.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 const MovieItem = ({ movie, id }) => {
   return (
-    <div className="movie-item flex flex-col items-center bg-white border border-gray-300 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out p-2">
+    <div className="">
       <Link to={`/movie/${id}`} className="w-full">
         <img
           src={movie.imageUrl}
           alt={movie.name}
-          className="w-48 h-72 object-cover rounded-lg"
+          className="w-48 h-72 object-cover rounded-lg m-5"
         />
         <h3 className="text-lg font-semibold text-gray-800 mt-2 text-center">{movie.name}</h3>
       </Link>
