8eadf648a53eea3e6529c122e349181d33eb0a8b
Brianlucca <brianlucca2002@gmail.com>
2024-08-08 02:24:31 -0300

    styles: added Style for scroll bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AuthProvider } from "./contexts/authContext/AuthContext";
 import RenderRoutes from "./routes/Routes";
+import './styles/Global.css'
 
 function App() {
   return (
