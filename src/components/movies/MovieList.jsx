e4d241e855b87ebd71d4a26d5a38b9334b952ba6
Brianlucca <brianlucca2002@gmail.com>
2024-08-08 02:36:56 -0300

    feat: Add MovieList component with Carousel integration
    - Implements the MovieList component to fetch and display movies.
    - Integrates the MovieItem component and Carousel for carousel display.

--- src/components/movies/MovieList.jsx
+++ src/components/movies/MovieList.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
-import { fetchMovies } from "../../services/movieService/MovieService";
-import MovieItem from "./MovieItem";
+import { useEffect, useState } from 'react';
+import { fetchMovies } from '../../services/movieService/MovieService';
+import MovieItem from './MovieItem';
+import Carousel from '../carousel/Carousel'; 
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
@@ -9,20 +10,25 @@ const MovieList = () => {
     const getMovies = async () => {
       try {
         const moviesData = await fetchMovies();
-        setMovies(Object.entries(moviesData)); 
+        setMovies(Object.entries(moviesData));
       } catch (error) {
-        console.error("Erro ao buscar filmes:", error);
+        console.error('Erro ao buscar filmes:', error);
       }
     };
 
     getMovies();
   }, []);
 
+  const movieItems = movies.map(([id, movie]) => (
+    <div key={id} className="w-60 flex-shrink-0">
+      <MovieItem movie={movie} id={id} />
+    </div>
+  ));
+
   return (
-    <div className="movie-list flex overflow-x-auto py-4 space-x-4">
-      {movies.map(([id, movie]) => (
-        <MovieItem key={id} movie={movie} id={id} />
-      ))}
+    <div className="ml-16 lg:ml-20 p-4">
+      <h1 className="text-2xl font-bold mb-4">Lista de Filmes</h1>
+      <Carousel items={movieItems} />
     </div>
   );
 };
