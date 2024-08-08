a7e17c701d21337a046698eeb27f5db1d24804fb
Brianlucca <brianlucca2002@gmail.com>
2024-08-08 02:40:51 -0300

    feat: implement admin page with sidebar, movie form, recommendation list, and loading state

--- src/pages/admin/AdminPage.jsx
+++ src/pages/admin/AdminPage.jsx
@@ -1,13 +1,17 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import AddMovieForm from "../../components/admin/AddMovieForm";
+import AddMovieForm from "../../components/admin/addMovieForm/AddMovieForm"; 
 import { AuthContext } from "../../contexts/authContext/AuthContext";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Footer from "../../components/footer/Footer";
+import RecommendationList from "../../components/admin/recommendationAdmin/RecommendationList";
+import Loading from "../../components/loading/Loading";
 
 const AdminPage = () => {
   const { isAdmin } = useContext(AuthContext);
 
   if (isAdmin === undefined) {
-    return <div>Loading...</div>;
+    return <Loading />
   }
 
   if (!isAdmin) {
@@ -15,8 +19,11 @@ const AdminPage = () => {
   }
 
   return (
-    <div>
+    <div className="bg-gray-100">
+      <Sidebar />
       <AddMovieForm />
+      <RecommendationList />
+      <Footer />
     </div>
   );
 };
