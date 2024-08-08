488dfad2cedcc102472a187e2a2da9455b870c0d
Brianlucca <brianlucca2002@gmail.com>
2024-08-08 02:27:03 -0300

    feat: add user registration, login, admin login, and logout functions

--- src/services/authService/AuthService.js
+++ src/services/authService/AuthService.js
@@ -1,11 +1,66 @@
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
 } from 'firebase/auth'
+import { getDatabase, ref, set } from 'firebase/database'
 import { auth } from '../firebaseConfig/FirebaseConfig'
 
-export const registerUser = (email, password) =>
-  createUserWithEmailAndPassword(auth, email, password)
+export const registerUser = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user
+      console.log('Usuário registrado:', user)
 
-export const loginUser = (email, password) =>
-  signInWithEmailAndPassword(auth, email, password)
+      const db = getDatabase()
+      return set(ref(db, 'roles/' + user.uid), 'user')
+    })
+    .catch((error) => {
+      console.error('Erro ao registrar usuário:', error)
+      throw error
+    })
+}
+
+export const loginUser = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user
+      console.log('Usuário logado:', user)
+      return user
+    })
+    .catch((error) => {
+      console.error('Erro ao fazer login:', error)
+      throw error
+    })
+}
+
+export const loginAdmin = () => {
+  const adminEmail = import.meta.env.VITE_ADMIN_EMAIL
+  const adminPassword = import.meta.env.VITE_ADMIN_PASSWORD
+
+  return loginUser(adminEmail, adminPassword)
+    .then((user) => {
+      const db = getDatabase()
+
+      return set(ref(db, 'roles/' + user.uid), 'admin').then(() => {
+        console.log('Papel de admin definido para o usuário:', user.uid)
+        return user
+      })
+    })
+    .catch((error) => {
+      console.error('Erro ao fazer login como admin:', error)
+      throw error
+    })
+}
+
+export const logout = () => {
+  return signOut(auth)
+    .then(() => {
+      console.log('Usuário deslogado com sucesso')
+      localStorage.clear()
+    })
+    .catch((error) => {
+      console.error('Erro ao fazer logout:', error)
+      throw error
+    })
+}
