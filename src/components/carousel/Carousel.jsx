4d65db97e648dc6bc84d8cc42a238eca5b4e71dc
Brianlucca <brianlucca2002@gmail.com>
2024-08-09 01:55:21 -0300

    refactor: changed the design

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -18,19 +18,19 @@ const Carousel = ({ items }) => {
   return (
     <div className="relative overflow-hidden">
       <button
-        className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-600 text-white p-2 w-10 rounded-full z-10 transition-transform duration-300 ease-in-out hover:bg-gray-700"
+        className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-transparent text-black text-3xl p-2 w-10 rounded-md z-10 transition-transform duration-300 ease-in-out hover:bg-gray-500 hover:text-white"
         onClick={scrollLeft}
       >
         &#10094;
       </button>
       <div
         ref={scrollRef}
-        className="carousel flex overflow-x-auto space-x-4"
+        className="carousel flex overflow-x-auto"
       >
         {items}
       </div>
       <button
-        className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-600 text-white  p-2 w-10 rounded-full z-10 transition-transform duration-300 ease-in-out hover:bg-gray-700"
+        className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-transparent text-black text-3xl  p-2 w-10 rounded-md z-10 transition-transform duration-300 ease-in-out hover:bg-gray-500 hover:text-white"
         onClick={scrollRight}
       >
         &#10095;
