0b8200ce09c9077c19056391ee4b0c83d810a44e
Brianlucca <brianlucca2002@gmail.com>
2024-08-09 01:52:00 -0300

    feat: added Toastify Global Import

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { AuthProvider } from "./contexts/authContext/AuthContext";
 import RenderRoutes from "./routes/Routes";
 import './styles/Global.css'
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
