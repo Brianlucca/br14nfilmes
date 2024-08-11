3b1d0f4ceb3d8db398f0af602a57f5d1229715ed
Brianlucca <brianlucca2002@gmail.com>
2024-08-10 21:44:31 -0300

    feat: Add content submission form with type-specific categories and admin check
    - Implemented a form to add content (movies, music, anime, series) with dynamic categories based on content type.
    
    - Added user role verification to ensure only admins can add content.
    
    - Included error handling and success notifications using toast.
    
    - Updated form fields for content data including name, image URL, YouTube link, description, rating, type, and category.

--- src/components/admin/addMovieForm/AddMovieForm.jsx
+++ src/components/admin/addMovieForm/AddMovieForm.jsx
@@ -1,19 +1,25 @@
-import { get, getDatabase, ref } from "firebase/database";
+import { get, ref, getDatabase } from "firebase/database";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../contexts/authContext/AuthContext";
 import { addMovie } from "../../../services/movieService/MovieService";
+import { addMusic } from "../../../services/musicService/MusicService";
+import { addAnime } from "../../../services/animeService/AnimeService";
+import { addSeries } from "../../../services/seriesService/SeriesService";
 import { toast } from 'react-toastify';
 
-const AddMovieForm = () => {
+const AddContentForm = () => {
   const { user } = useContext(AuthContext);
-  const [movieData, setMovieData] = useState({
+  const [contentData, setContentData] = useState({
     name: "",
     imageUrl: "",
     youtubeLink: "",
     description: "",
-    rating: ""
+    rating: "",
+    type: "",
+    category: ""
   });
   const [isUserAdmin, setIsUserAdmin] = useState(false);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     if (user) {
@@ -29,48 +35,98 @@ const AddMovieForm = () => {
   }, [user]);
 
   const handleChange = (e) => {
-    setMovieData({ ...movieData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+
+    if (name === 'type') {
+      let selectedCategories = [];
+      switch (value) {
+        case 'movie':
+          selectedCategories = ['Ação', 'Comédia', 'Drama', 'Terror', 'Romance', 'Ficção Científica'];
+          break;
+        case 'music':
+          selectedCategories = ['Rock', 'Pop', 'Hip-hop', 'Jazz', 'Clássica', 'Eletrônica'];
+          break;
+        case 'anime':
+          selectedCategories = ['Shounen', 'Seinen', 'Shojo', 'Isekai', 'Mecha', 'Slice of Life'];
+          break;
+        case 'serie':
+          selectedCategories = ['Drama', 'Comédia', 'Mistério', 'Suspense', 'Sci-Fi', 'Fantasia'];
+          break;
+        default:
+          selectedCategories = [];
+      }
+      setCategories(selectedCategories);
+
+      setContentData((prevData) => ({
+        ...prevData,
+        type: value,
+        category: ""
+      }));
+    } else {
+      setContentData({ ...contentData, [name]: value });
+    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!user || !isUserAdmin) {
+      toast.error("Você precisa ser um administrador para adicionar conteúdo.");
       return;
     }
 
     try {
-      const movieWithUser = { 
-        ...movieData, 
+      const contentWithUser = { 
+        ...contentData, 
         userEmail: user.email
       };
-      await addMovie(movieWithUser);
-      toast.success(`Filme "${movieData.name}" foi adicionado.`);
-      setMovieData({
+
+      switch (contentData.type) {
+        case 'movie':
+          await addMovie(contentWithUser);
+          break;
+        case 'music':
+          await addMusic(contentWithUser);
+          break;
+        case 'anime':
+          await addAnime(contentWithUser);
+          break;
+        case 'serie':
+          await addSeries(contentWithUser);
+          break;
+        default:
+          throw new Error('Tipo de conteúdo inválido.');
+      }
+
+      toast.success(`"${contentData.name}" foi adicionado.`);
+      setContentData({
         name: "",
         imageUrl: "",
         youtubeLink: "",
         description: "",
-        rating: ""
+        rating: "",
+        type: "",
+        category: ""
       });
+      setCategories([]);
     } catch (error) {
-      toast.error("Ocorreu um erro ao adicionar o filme. Tente novamente.");
+      toast.error("Ocorreu um erro ao adicionar o conteúdo. Tente novamente.");
     }
   };
 
   return (
     <div className="w-full bg-white shadow-xl rounded-lg p-6">
-      <h2 className="text-2xl sm:text-3xl font-bold mb-6 text-gray-900 text-center">Adicionar Filme</h2>
+      <h2 className="text-2xl sm:text-3xl font-bold mb-6 text-gray-900 text-center">Adicionar Conteúdo</h2>
       
       <form onSubmit={handleSubmit} className="space-y-6">
         <div className="mb-4">
-          <label htmlFor="name" className="block text-gray-700 text-sm sm:text-base font-medium mb-2">Nome do Filme</label>
+          <label htmlFor="name" className="block text-gray-700 text-sm sm:text-base font-medium mb-2">Nome</label>
           <input
             id="name"
             name="name"
             type="text"
-            value={movieData.name}
+            value={contentData.name}
             onChange={handleChange}
-            placeholder="Nome do Filme"
+            placeholder="Nome"
             className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
@@ -81,7 +137,7 @@ const AddMovieForm = () => {
             id="imageUrl"
             name="imageUrl"
             type="text"
-            value={movieData.imageUrl}
+            value={contentData.imageUrl}
             onChange={handleChange}
             placeholder="Link da Imagem"
             className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -94,7 +150,7 @@ const AddMovieForm = () => {
             id="youtubeLink"
             name="youtubeLink"
             type="text"
-            value={movieData.youtubeLink}
+            value={contentData.youtubeLink}
             onChange={handleChange}
             placeholder="Link do Trailer"
             className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -106,7 +162,7 @@ const AddMovieForm = () => {
           <textarea
             id="description"
             name="description"
-            value={movieData.description}
+            value={contentData.description}
             onChange={handleChange}
             placeholder="Descrição"
             rows="4"
@@ -120,22 +176,55 @@ const AddMovieForm = () => {
             id="rating"
             name="rating"
             type="text"
-            value={movieData.rating}
+            value={contentData.rating}
             onChange={handleChange}
             placeholder="Avaliação"
             className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
+        </div>
+
+        <div className="mb-6">
+          <label htmlFor="type" className="block text-gray-700 text-sm sm:text-base font-medium mb-2">Tipo</label>
+          <select
+            id="type"
+            name="type"
+            value={contentData.type}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            <option value="">Selecione um tipo</option>
+            <option value="movie">Filme</option>
+            <option value="music">Música</option>
+            <option value="anime">Anime</option>
+            <option value="serie">Série</option>
+          </select>
+        </div>
+
+        <div className="mb-6">
+          <label htmlFor="category" className="block text-gray-700 text-sm sm:text-base font-medium mb-2">Categoria</label>
+          <select
+            id="category"
+            name="category"
+            value={contentData.category}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            <option value="">Selecione uma categoria</option>
+            {categories.map((category, index) => (
+              <option key={index} value={category}>{category}</option>
+            ))}
+          </select>
         </div>
         
         <button
           type="submit"
           className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
-          Adicionar Filme
+          Adicionar Conteúdo
         </button>
       </form>
     </div>
   );
 };
 
-export default AddMovieForm;
+export default AddContentForm;
