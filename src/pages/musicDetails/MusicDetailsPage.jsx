4d5fa65dcb6bd3b5773ede2c9276a16e3b26e1ca
Brianlucca <brianlucca2002@gmail.com>
2024-08-14 00:00:54 -0300

    feat: added route to create session

--- src/pages/musicDetails/MusicDetailsPage.jsx
+++ src/pages/musicDetails/MusicDetailsPage.jsx
@@ -88,6 +88,11 @@ const MusicDetailsPage = () => {
     }
   };
 
+  const handleCreateSession = () => {
+    navigate(`/create-session/${id}`)
+  }
+
+
   if (!music) {
     return <Loading />;
   }
@@ -123,8 +128,10 @@ const MusicDetailsPage = () => {
               <button className="px-4 py-2 bg-red-500 text-white font-semibold rounded-md shadow-md hover:bg-red-600 mb-2 sm:mb-0">
                 ❤️ Favoritos
               </button>
-              <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-600">
-                📺 Assistir Mais Tarde
+              <button
+              onClick={handleCreateSession}
+              className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-600">
+                📺 Assistir em Grupo
               </button>
             </div>
           </div>
