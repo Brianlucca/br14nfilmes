596b8fd6acb96a780015c9b067e5a3623d467b07
Brianlucca <brianlucca2002@gmail.com>
2024-09-20 15:48:10 -0300

    fix: removed part of the logic and added it to the hooks folder for project organization

--- src/pages/animeDetails/AnimeDetailsPage.jsx
+++ src/pages/animeDetails/AnimeDetailsPage.jsx
@@ -1,133 +1,24 @@
-import { get, ref, onValue, remove, push, set } from "firebase/database";
-import { useEffect, useState, useContext } from "react";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import Comments from "../../components/comments/Comments";
 import Footer from "../../components/footer/Footer";
 import Loading from "../../components/loading/Loading";
 import Sidebar from "../../components/sidebar/Sidebar";
-import { database } from "../../services/firebaseConfig/FirebaseConfig";
-import { AuthContext } from "../../contexts/authContext/AuthContext";
-import { ToastContainer, toast } from "react-toastify";
-import { Calendar, Clapperboard, Star, Heart, HeartOff, Popcorn} from "lucide-react";
+import useAnimeDetails from "../../hooks/useAnimeDetails/useAnimeDetails";
+import { ToastContainer } from "react-toastify";
+import { Calendar, Clapperboard, Star, Heart, HeartOff, Popcorn } from "lucide-react";
 
 const AnimeDetailsPage = () => {
   const { id } = useParams();
-  const [anime, setAnime] = useState(null);
-  const [comments, setComments] = useState([]);
-  const [isFavorite, setIsFavorite] = useState(false);
-  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
-  const location = useLocation();
 
-  useEffect(() => {
-    const fetchAnimeDetails = async () => {
-      try {
-        const animeRef = ref(database, `animes/${id}`);
-        const snapshot = await get(animeRef);
-        if (snapshot.exists()) {
-          setAnime(snapshot.val());
-        } else {
-          toast.error("Anime não encontrado");
-        }
-      } catch (error) {
-        toast.error("Erro ao buscar detalhes do anime");
-      }
-    };
-
-    fetchAnimeDetails();
-  }, [id]);
-
-  useEffect(() => {
-    const commentsRef = ref(database, `animes/${id}/comments`);
-    onValue(commentsRef, (snapshot) => {
-      const commentsData = snapshot.val();
-      if (commentsData) {
-        setComments(Object.entries(commentsData));
-      } else {
-        setComments([]);
-      }
-    });
-  }, [id]);
-
-  useEffect(() => {
-    if (user) {
-      const favoritesRef = ref(database, `users/${user.uid}/favorites/anime/${id}`);
-      onValue(favoritesRef, (snapshot) => {
-        setIsFavorite(snapshot.exists());
-      });
-    }
-  }, [id, user]);
-
-  const handleCommentSubmit = async (commentText, replyingTo) => {
-    if (!user) {
-      toast.error("Usuário não autenticado");
-      return;
-    }
-
-    const roleRef = ref(database, `users/${user.uid}/updateNick/${user.uid}`);
-    const roleSnapshot = await get(roleRef);
-    const roleData = roleSnapshot.val();
-
-    if (!roleData || !roleData.nickname) {
-      toast.warn("Por favor, crie um nickname antes de comentar.");
-      navigate("/profile", { state: { from: location.pathname } });
-      return;
-    }
-
-    const comment = {
-      text: commentText,
-      userName: roleData.nickname,
-      createdAt: new Date().toISOString(),
-      replyingToName: replyingTo
-        ? comments.find(([key]) => key === replyingTo)[1].userName
-        : null,
-    };
-
-    try {
-      await push(ref(database, `animes/${id}/comments`), comment);
-      toast.success("Comentário enviado com sucesso!");
-    } catch (error) {
-      toast.error("Erro ao enviar comentário");
-    }
-  };
-
-  const handleDeleteComment = async (commentKey) => {
-    try {
-      await remove(ref(database, `animes/${id}/comments/${commentKey}`));
-      toast.success("Comentário deletado com sucesso!");
-    } catch (error) {
-      toast.error("Erro ao deletar comentário");
-    }
-  };
-
-  const toggleFavorite = async () => {
-    if (!user) {
-      toast.error("Usuário não autenticado");
-      return;
-    }
-
-    const favoriteRef = ref(database, `users/${user.uid}/favorites/anime/${id}`);
-
-    try {
-      if (isFavorite) {
-        await remove(favoriteRef);
-        toast.success("Anime removido dos favoritos");
-        setIsFavorite(false);
-      } else {
-        await set(favoriteRef, {
-          id,
-          title: anime.name,
-          imageUrl: anime.imageUrl,
-          gif: anime.gif,
-          addedAt: new Date().toISOString(),
-        });
-        toast.success("Anime adicionado aos favoritos");
-        setIsFavorite(true);
-      }
-    } catch (error) {
-      toast.error("Erro ao atualizar favoritos");
-    }
-  };
+  const {
+    anime,
+    comments,
+    isFavorite,
+    handleCommentSubmit,
+    handleDeleteComment,
+    toggleFavorite,
+  } = useAnimeDetails(id);
 
   const handleCreateSession = () => {
     navigate(`/create-session/${id}`);
@@ -190,7 +81,7 @@ const AnimeDetailsPage = () => {
                   className="px-4 py-2 bg-[#605f5f] text-white font-semibold rounded-md shadow-md hover:bg-blue-600 flex items-center justify-center"
                 >
                   <Popcorn className="text-white mr-2" />
-                  <span>Assistir</span>
+                  <span>Criar Sessão</span>
                 </button>
               </div>
               {anime.youtubeLink && (
--- src/pages/seriesDetails/SeriesDetailsPage.jsx
+++ src/pages/seriesDetails/SeriesDetailsPage.jsx
@@ -1,141 +1,33 @@
-import { get, ref, onValue, remove, push, set } from "firebase/database";
-import { useEffect, useState, useContext } from "react";
-import { useParams, useNavigate, useLocation } from "react-router-dom";
+import { Calendar, Clapperboard, Star, Heart, HeartOff, Popcorn } from "lucide-react";
+import { useNavigate, useParams } from "react-router-dom";
+import useSeriesDetails from "../../hooks/useSerieDetails/useSerieDetails";
 import Comments from "../../components/comments/Comments";
 import Footer from "../../components/footer/Footer";
 import Loading from "../../components/loading/Loading";
 import Sidebar from "../../components/sidebar/Sidebar";
-import { database } from "../../services/firebaseConfig/FirebaseConfig";
-import { AuthContext } from "../../contexts/authContext/AuthContext";
-import { ToastContainer, toast } from "react-toastify";
-import { Calendar, Clapperboard, Star, Heart, HeartOff, Popcorn } from "lucide-react";
+import { ToastContainer } from "react-toastify";
 
 const SeriesDetailsPage = () => {
   const { id } = useParams();
-  const [series, setSeries] = useState(null);
-  const [comments, setComments] = useState([]);
-  const [isFavorite, setIsFavorite] = useState(false);
-  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
-  const location = useLocation();
 
-  useEffect(() => {
-    const fetchSeriesDetails = async () => {
-      try {
-        const seriesRef = ref(database, `series/${id}`);
-        const snapshot = await get(seriesRef);
-        if (snapshot.exists()) {
-          setSeries(snapshot.val());
-        } else {
-          toast.error("Série não encontrada");
-        }
-      } catch (error) {
-        toast.error("Erro ao buscar detalhes da série");
-      }
-    };
 
-    fetchSeriesDetails();
-  }, [id]);
-
-  useEffect(() => {
-    const commentsRef = ref(database, `series/${id}/comments`);
-    onValue(commentsRef, (snapshot) => {
-      const commentsData = snapshot.val();
-      if (commentsData) {
-        setComments(Object.entries(commentsData));
-      } else {
-        setComments([]);
-      }
-    });
-  }, [id]);
-
-  useEffect(() => {
-    if (user) {
-      const favoritesRef = ref(database, `users/${user.uid}/favorites/serie/${id}`);
-      onValue(favoritesRef, (snapshot) => {
-        setIsFavorite(snapshot.exists());
-      });
-    }
-  }, [id, user]);
-
-  const handleCommentSubmit = async (commentText, replyingTo) => {
-    if (!user) {
-      toast.error("Usuário não autenticado");
-      return;
-    }
-
-    const roleRef = ref(database, `users/${user.uid}/updateNick/${user.uid}`);
-    const roleSnapshot = await get(roleRef);
-    const roleData = roleSnapshot.val();
-
-    if (!roleData || !roleData.nickname) {
-      toast.warn("Por favor, crie um nickname antes de comentar.");
-      navigate("/profile", { state: { from: location.pathname } });
-      return;
-    }
-
-    const comment = {
-      text: commentText,
-      userName: roleData.nickname,
-      createdAt: new Date().toISOString(),
-      replyingToName: replyingTo
-        ? comments.find(([key]) => key === replyingTo)[1].userName
-        : null,
-    };
-
-    try {
-      await push(ref(database, `series/${id}/comments`), comment);
-      toast.success("Comentário enviado com sucesso!");
-    } catch (error) {
-      toast.error("Erro ao enviar comentário");
-    }
-  };
-
-  const handleDeleteComment = async (commentKey) => {
-    try {
-      await remove(ref(database, `series/${id}/comments/${commentKey}`));
-      toast.success("Comentário deletado com sucesso!");
-    } catch (error) {
-      toast.error("Erro ao deletar comentário");
-    }
-  };
-
-  const toggleFavorite = async () => {
-    if (!user) {
-      toast.error("Usuário não autenticado");
-      return;
-    }
-  
-    const favoriteRef = ref(database, `users/${user.uid}/favorites/serie/${id}`);
-  
-    try {
-      if (isFavorite) {
-        await remove(favoriteRef);
-        toast.success("Série removida dos favoritos");
-        setIsFavorite(false);
-      } else {
-        await set(favoriteRef, {
-          id,
-          title: series.name,
-          imageUrl: series.imageUrl,
-          gif: series.gif,
-          addedAt: new Date().toISOString(),
-        });
-        toast.success("Série adicionada aos favoritos");
-        setIsFavorite(true);
-      }
-    } catch (error) {
-      toast.error("Erro ao atualizar favoritos");
-    }
-  };
-  
-  const handleCreateSession = () => {
-    navigate(`/create-session/${id}`);
-  };
+  const {
+    series,
+    comments,
+    isFavorite,
+    handleCommentSubmit,
+    handleDeleteComment,
+    toggleFavorite,
+  } = useSeriesDetails(id);
 
   if (!series) {
     return <Loading />;
   }
+
+  const handleCreateSession = () => {
+    navigate(`/create-session/${id}`);
+  };
 
   return (
     <div>
@@ -190,7 +82,7 @@ const SeriesDetailsPage = () => {
                   className="px-4 py-2 bg-[#605f5f] text-white font-semibold rounded-md shadow-md hover:bg-blue-600 flex items-center justify-center"
                 >
                   <Popcorn className="text-white mr-2" />
-                  <span>Assistir</span>
+                  <span>Criar Sessão</span>
                 </button>
               </div>
               {series.youtubeLink && (
