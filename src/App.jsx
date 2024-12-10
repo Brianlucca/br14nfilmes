d564929fb61f9be5dba8b699e80c705c1d6cf98e
Brianlucca <brianlucca2002@gmail.com>
2024-12-10 16:39:29 -0300

    refactor: added browserRouter out of context

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/authContext/AuthContext";
 import RenderRoutes from "./routes/Routes";
-import { ToastContainer } from 'react-toastify';
-import './styles/Global.css'
-import 'react-toastify/dist/ReactToastify.css';
-
+import { ToastContainer } from "react-toastify";
+import "./styles/Global.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <AuthProvider>
-      <RenderRoutes />
-      <ToastContainer />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+          <RenderRoutes />
+          <ToastContainer />
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
