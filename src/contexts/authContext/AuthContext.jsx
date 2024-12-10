c070fdbdd341a91315dee226067295ae2c1cec66
Brianlucca <brianlucca2002@gmail.com>
2024-12-10 16:42:43 -0300

    feat: added code in context to verify email

--- src/contexts/authContext/AuthContext.jsx
+++ src/contexts/authContext/AuthContext.jsx
@@ -2,6 +2,7 @@ import { onAuthStateChanged } from "firebase/auth";
 import { get, getDatabase, ref, set } from "firebase/database";
 import { createContext, useEffect, useState } from "react";
 import { auth } from "../../services/firebaseConfig/FirebaseConfig";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
@@ -9,12 +10,24 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isAdmin, setIsAdmin] = useState(false);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const adminEmail = import.meta.env.VITE_ADMIN_EMAIL;
 
     const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      setLoading(true);
+
       if (currentUser) {
+        await currentUser.reload();
+        if (!currentUser.emailVerified) {
+          setUser(null);
+          setIsAdmin(false);
+          navigate("/verify-email");
+          setLoading(false);
+          return;
+        }
+
         setUser(currentUser);
         const adminStatus = currentUser.email === adminEmail;
         setIsAdmin(adminStatus);
@@ -51,7 +64,7 @@ export const AuthProvider = ({ children }) => {
     });
 
     return () => unsubscribe();
-  }, []);
+  }, [navigate]);
 
   return (
     <AuthContext.Provider value={{ user, isAdmin, loading }}>
