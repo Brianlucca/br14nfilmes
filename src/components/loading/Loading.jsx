26d30fc1b1a709fc8be2d50d4d4568f0bb56d0f8
Brian Lucca <brianlucca2002@gmail.com>
2025-05-28 15:43:07 -0300

    refactor: refactored design

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,11 +1,16 @@
+import React from 'react';
 import './Style.css';
 
 const Loading = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-black">
-      <div className="flex flex-col justify-center items-center space-y-4">
-        <div className="loader"></div>
-        <p className="text-gray-400">Carregando...</p>
+      <div className="flex flex-col justify-center items-center space-y-5"> 
+        <div className="pulsing-dots-loader">
+          <span style={{ backgroundColor: 'rgb(14 165 233)' }}></span> 
+          <span style={{ backgroundColor: 'rgb(14 165 233)' }}></span> 
+          <span style={{ backgroundColor: 'rgb(14 165 233)' }}></span> 
+        </div>
+        <p className="text-slate-400 text-sm">Carregando...</p>
       </div>
     </div>
   );
